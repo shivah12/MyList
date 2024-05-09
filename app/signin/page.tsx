b3c5f0cf7a79af351f9619c7532b7b1d1772488b
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-05-09 18:48:25 +0530

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { SignIn } from "@clerk/nextjs";
 import React from "react";
-import dotenv from 'dotenv';
 
-dotenv.config();
 function page() {
   return (
     <div className="flex items-center justify-center h-full">
